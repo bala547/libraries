bd593c7e9bb6c34b7ec19b6e276605fb64dc3c77
bala547 <balakrishnaulli3@gmail.com>
2024-11-20 18:24:07 +0530

    Update runFlywayMigrations.groovy

--- runflywaymigrations/steps/runFlywayMigrations.groovy
+++ runflywaymigrations/steps/runFlywayMigrations.groovy
@@ -1,13 +1,29 @@
 void call() {
     node {
-    def flywayCommand = """
-                          ./flyway/flyway \
-                          -url=jdbc:snowflake://${SNOWFLAKE_URL} \
-                          -user=${SNOWFLAKE_USERNAME} \
-                          -password=${SNOWFLAKE_PASSWORD} \
-                          -locations=filesystem:migrations \
-                           migrate
-                           """
-                        sh "${flywayCommand}"
+        // Make sure you have the necessary environment variables for Snowflake
+        def snowflakeUrl = "jdbc:snowflake://${SNOWFLAKE_URL}"
+        def snowflakeUsername = "${SNOWFLAKE_USERNAME}"
+        def snowflakePassword = "${SNOWFLAKE_PASSWORD}"
+        def migrationPath = "filesystem:migrations"  // Ensure this path is correct
+
+        // Flyway command string to execute
+        def flywayCommand = """
+            ./flyway/flyway \
+            -url=${snowflakeUrl} \
+            -user=${snowflakeUsername} \
+            -password=${snowflakePassword} \
+            -locations=${migrationPath} \
+            migrate
+        """
+
+        // Ensure the migration folder and Flyway command are set up correctly
+        echo "Running Flyway migrations..."
+        
+        // Execute the command using 'sh' to run it in the shell
+        sh "${flywayCommand}"
+
+        // Optionally, you can add a step to verify if Flyway ran successfully
+        // (e.g., checking the migration status)
+        sh "./flyway/flyway -url=${snowflakeUrl} -user=${snowflakeUsername} -password=${snowflakePassword} info"
     }
 }
