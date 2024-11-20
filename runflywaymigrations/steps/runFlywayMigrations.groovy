17b96ec99e3b2455b7059ec19678fcb33cf74622
bala547 <balakrishnaulli3@gmail.com>
2024-11-20 16:55:02 +0530

    Update runFlywayMigrations.groovy

--- runflywaymigrations/steps/runFlywayMigrations.groovy
+++ runflywaymigrations/steps/runFlywayMigrations.groovy
@@ -1,14 +1,17 @@
 void call() {
     node {
-        // Construct the Flyway command as a single line
-        def flywayCommand = "./flyway/flyway -url=jdbc:snowflake://${SNOWFLAKE_URL}?warehouse=${SNOWFLAKE_WAREHOUSE}&database=${SNOWFLAKE_DATABASE}&role=${SNOWFLAKE_ROLE} -user=${SNOWFLAKE_USERNAME} -password=${SNOWFLAKE_PASSWORD} -schemas=BALA -table=FLYWAY_SCHEMA_HISTORY -locations=filesystem:migrations migrate"
-        
+        // Assuming SNOWFLAKE_URL includes 'https://', we remove the prefix 'https://' from SNOWFLAKE_URL if necessary
+        def flywayUrl = "jdbc:snowflake://${SNOWFLAKE_URL}?warehouse=${SNOWFLAKE_WAREHOUSE}&database=${SNOWFLAKE_DATABASE}&role=${SNOWFLAKE_ROLE}"
+
+        // Construct the Flyway command as a single string
+        def flywayCommand = "./flyway/flyway -url=${flywayUrl} -user=${SNOWFLAKE_USERNAME} -password=${SNOWFLAKE_PASSWORD} -schemas=BALA -table=FLYWAY_SCHEMA_HISTORY -locations=filesystem:migrations migrate"
+
         // Print the Flyway command for debugging purposes
         echo "Running Flyway command: ${flywayCommand}"
-        
-        // Ensure that the command is executable and present in the correct path
+
+        // Ensure that the Flyway executable exists and is executable
         sh 'ls -l ./flyway/flyway'  // Check if the Flyway executable exists
-        
+
         // Run the Flyway migration command
         sh "${flywayCommand}"
     }
