be710f124b421b08ae7a0bb35d87c910253ebb16
bala547 <balakrishnaulli3@gmail.com>
2024-11-20 16:41:43 +0530

    Update runFlywayMigrations.groovy

--- runflywaymigrations/steps/runFlywayMigrations.groovy
+++ runflywaymigrations/steps/runFlywayMigrations.groovy
@@ -2,11 +2,11 @@ void call() {
     node {
         // Construct the Flyway command
         def flywayCommand = "./flyway/flyway -url=jdbc:snowflake://${SNOWFLAKE_URL}?warehouse=${SNOWFLAKE_WAREHOUSE}&database=${SNOWFLAKE_DATABASE}&role=${SNOWFLAKE_ROLE} -user=${SNOWFLAKE_USERNAME} -password=${SNOWFLAKE_PASSWORD} -schemas=BALA -table=FLYWAY_SCHEMA_HISTORY -locations=filesystem:migrations migrate"
-        
-        // Print the command for debugging purposes
+
+        // Print the Flyway command for debugging
         echo "Running Flyway command: ${flywayCommand}"
 
         // Execute the Flyway command
-        sh "${flywayCommand}"
+        sh flywayCommand
     }
 }
