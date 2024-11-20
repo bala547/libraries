e565e9d5ead84af7fb24b9eabbef09243b0761a6
bala547 <balakrishnaulli3@gmail.com>
2024-11-20 16:59:35 +0530

    Update runFlywayMigrations.groovy

--- runflywaymigrations/steps/runFlywayMigrations.groovy
+++ runflywaymigrations/steps/runFlywayMigrations.groovy
@@ -1,9 +1,9 @@
 void call() {
     node {
-        // Assuming SNOWFLAKE_URL includes 'https://', we remove the prefix 'https://' from SNOWFLAKE_URL if necessary
+        // Assuming SNOWFLAKE_URL includes 'https://', use it directly in the JDBC URL
         def flywayUrl = "jdbc:snowflake://${SNOWFLAKE_URL}?warehouse=${SNOWFLAKE_WAREHOUSE}&database=${SNOWFLAKE_DATABASE}&role=${SNOWFLAKE_ROLE}"
 
-        // Construct the Flyway command as a single string
+        // Construct the Flyway command as a single string without unintended breaks or spaces
         def flywayCommand = "./flyway/flyway -url=${flywayUrl} -user=${SNOWFLAKE_USERNAME} -password=${SNOWFLAKE_PASSWORD} -schemas=BALA -table=FLYWAY_SCHEMA_HISTORY -locations=filesystem:migrations migrate"
 
         // Print the Flyway command for debugging purposes
@@ -12,7 +12,7 @@ void call() {
         // Ensure that the Flyway executable exists and is executable
         sh 'ls -l ./flyway/flyway'  // Check if the Flyway executable exists
 
-        // Run the Flyway migration command
-        sh "${flywayCommand}"
+        // Run the Flyway migration command as a single string
+        sh """${flywayCommand}"""
     }
 }
