c44db3d0aecc165245c151241382ed741c68e4fd
bala547 <balakrishnaulli3@gmail.com>
2024-11-21 14:50:31 +0530

    Update runFlywayMigrations.groovy

--- runflywaymigrations/steps/runFlywayMigrations.groovy
+++ runflywaymigrations/steps/runFlywayMigrations.groovy
@@ -1,19 +1,15 @@
 void call() {
     node {
-        def flywayImage = 'flyway/flyway:10.17.3' // Specific Flyway image version to avoid potential issues
+        def flywayImage = 'flyway/flyway:10.17.3' 
 
         echo "Running Flyway Migrations..."
 
-        // Ensure the necessary environment variable is set before running Flyway migration
+        // Explicitly pass Java options for the Docker container
         withEnv(["JAVA_TOOL_OPTIONS=--add-opens=java.base/java.nio=ALL-UNNAMED"]) {
-            
-            // Log the environment variable for debugging purposes
-            echo "JAVA_TOOL_OPTIONS=${env.JAVA_TOOL_OPTIONS}"
-
-            // Use the correct escaping and ensure variables are correctly passed to Docker command
             sh """
                 docker run --rm \\
                     -v \$(pwd)/migrations:/flyway/sql \\
+                    -e JAVA_TOOL_OPTIONS=--add-opens=java.base/java.nio=ALL-UNNAMED \\
                     ${flywayImage} \\
                     -url=jdbc:snowflake://${SNOWFLAKE_URL} \\
                     -user=${SNOWFLAKE_USERNAME} \\
