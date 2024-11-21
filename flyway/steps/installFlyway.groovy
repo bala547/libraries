a40cddd5835848df5e1a7e546aa71ce28749bbf2
bala547 <balakrishnaulli3@gmail.com>
2024-11-21 10:42:57 +0530

    Update installFlyway.groovy

--- flyway/steps/installFlyway.groovy
+++ flyway/steps/installFlyway.groovy
@@ -1,13 +1,10 @@
 def call() {
     node {
-        // Set the JVM options required for Flyway and Snowflake JDBC
-        withEnv(["JAVA_TOOL_OPTIONS=--add-opens=java.base/java.nio=ALL-UNNAMED"]) {
             // Your Flyway logic here (e.g., downloading Flyway, running migrations)
              sh "rm -rf flyway"
              sh "wget https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/10.17.3/flyway-commandline-10.17.3-linux-x64.tar.gz"  
              sh "tar xzvf flyway-commandline-10.17.3-linux-x64.tar.gz"
              sh "mv flyway-10.17.3 flyway"
              sh "rm flyway-commandline-10.17.3-linux-x64.tar.gz"
-        }
     }
 }
