0bed7762774e84d269c14390794fa3923dba413d
bala547 <balakrishnaulli3@gmail.com>
2024-11-21 15:05:17 +0530

    Update installFlyway.groovy

--- flyway/steps/installFlyway.groovy
+++ flyway/steps/installFlyway.groovy
@@ -1,10 +1,8 @@
 void call() {
     node {
-        withEnv(["JAVA_TOOL_OPTIONS=--add-opens=java.base/java.nio=ALL-UNNAMED"]) {
             def flywayImage = 'flyway/flyway:10.17.3'
             echo "Pulling Flyway Docker image: ${flywayImage}"
             sh "docker pull ${flywayImage}"
             echo "Flyway Docker image ${flywayImage} is ready for use"
-        }
     }
 }
