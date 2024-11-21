f2dbf97db1b967ed64dc7328a1e898b90bc6d149
bala547 <balakrishnaulli3@gmail.com>
2024-11-21 17:49:54 +0530

    Update runFlywayMigrations.groovy

--- runflywaymigrations/steps/runFlywayMigrations.groovy
+++ runflywaymigrations/steps/runFlywayMigrations.groovy
@@ -8,12 +8,10 @@ void call() {
                 docker run --rm \\
                     -v \$(pwd)/migrations:/flyway/sql \\
                     -e JAVA_TOOL_OPTIONS=--add-opens=java.base/java.nio=ALL-UNNAMED \\
-                    ${flywayImage}/flyway \\
-                    -url=jdbc:snowflake://${SNOWFLAKE_URL} \\
+                    ${flywayImage} \\
+                    -url=jdbc:snowflake://${SNOWFLAKE_URL}/?db=EMP&schema=BALA \\
                     -user=${SNOWFLAKE_USERNAME} \\
                     -password=${SNOWFLAKE_PASSWORD} \\
-                    -db=EMP \\
-                    -schema=BALA \\
                     -locations=filesystem:/flyway/sql \\
                     migrate
             """
