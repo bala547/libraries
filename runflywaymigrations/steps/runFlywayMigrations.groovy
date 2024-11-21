8ac10e3aaaa5c0e4cd964b402bd3c09845d71cbb
bala547 <balakrishnaulli3@gmail.com>
2024-11-21 13:14:30 +0530

    Update runFlywayMigrations.groovy

--- runflywaymigrations/steps/runFlywayMigrations.groovy
+++ runflywaymigrations/steps/runFlywayMigrations.groovy
@@ -1,7 +1,7 @@
 void call() {
     node {
         sh """
-        java --add-opens=java.base/java.nio=org.apache.arrow.memory.core,ALL-UNNAMED -jar flyway-commandline-<version>.jar \
+        java --add-opens=java.base/java.nio=org.apache.arrow.memory.core,ALL-UNNAMED -jar flyway-commandline-10.17.3.jar \
              -url=jdbc:snowflake://${SNOWFLAKE_URL} \
              -user=${SNOWFLAKE_USERNAME} \
              -password=${SNOWFLAKE_PASSWORD} \
