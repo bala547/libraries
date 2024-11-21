6df3e3c78dddfb001d9675e1d46d72312793e539
bala547 <balakrishnaulli3@gmail.com>
2024-11-21 13:39:38 +0530

    Update runFlywayMigrations.groovy

--- runflywaymigrations/steps/runFlywayMigrations.groovy
+++ runflywaymigrations/steps/runFlywayMigrations.groovy
@@ -1,13 +1,25 @@
 void call() {
     node {
-        sh """
-        java --add-opens=java.base/java.nio=org.apache.arrow.memory.core,ALL-UNNAMED \
-             -jar flyway-commandline-10.17.3.jar \
-             -url=jdbc:snowflake://${SNOWFLAKE_URL} \
-             -user=${SNOWFLAKE_USERNAME} \
-             -password=${SNOWFLAKE_PASSWORD} \
-             -locations=filesystem:migrations \
-             migrate
-        """
+        script {
+            // Define environment variables
+            def flywayJarPath = '/root/.jenkins/workspace/job2/flyway/lib/flyway/flyway-commandline-10.17.3.jar'
+            def snowflakeUrl = '${SNOWFLAKE_URL}'
+            def snowflakeUsername = '${SNOWFLAKE_USERNAME}'
+            def snowflakePassword = '${SNOWFLAKE_PASSWORD}'
+
+            // Ensure the Flyway JAR exists
+            if (!fileExists(flywayJarPath)) {
+                error "Flyway JAR not found at ${flywayJarPath}"
+            }
+
+            // Run the Flyway migration
+            sh """
+                java --add-opens=java.base/java.nio=org.apache.arrow.memory.core,ALL-UNNAMED -jar ${flywayJarPath} \
+                -url=jdbc:snowflake://${snowflakeUrl} \
+                -user=${snowflakeUsername} \
+                -password=${snowflakePassword} \
+                -locations=filesystem:migrations migrate
+            """
+        }
     }
 }
