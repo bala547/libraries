a629be1b4edbdeae1f09d391849f91ecc041f062
bala547 <balakrishnaulli3@gmail.com>
2024-11-21 14:30:46 +0530

    Update runFlywayMigrations.groovy

--- runflywaymigrations/steps/runFlywayMigrations.groovy
+++ runflywaymigrations/steps/runFlywayMigrations.groovy
@@ -1,15 +1,20 @@
 void call() {
- node {
-    sh """
-    docker run --rm \
-        -v \$(pwd)/migrations:/flyway/sql \
-        -v \$(pwd)/flyway.conf:/flyway/conf/flyway.conf \
-        flyway/flyway:latest \
-        -url=jdbc:snowflake://${SNOWFLAKE_URL} \
-        -user=${SNOWFLAKE_USERNAME} \
-        -password=${SNOWFLAKE_PASSWORD} \
-        -locations=filesystem:migrations \
-        migrate
-    """
-}
+    node {
+        def flywayImage = 'flyway/flyway:10.17.3' 
+
+        echo "Running Flyway Migrations..."
+
+        withEnv(["JAVA_TOOL_OPTIONS=--add-opens=java.base/java.nio=ALL-UNNAMED"]) {
+            sh """
+                docker run --rm \
+                    -v \$(pwd)/migrations:/flyway/sql \
+                    ${flywayImage} \
+                    -url=jdbc:snowflake://${SNOWFLAKE_URL} \
+                    -user=${SNOWFLAKE_USERNAME} \
+                    -password=${SNOWFLAKE_PASSWORD} \
+                    -locations=filesystem:/flyway/sql \
+                    migrate
+            """
+        }
+    }
 }
