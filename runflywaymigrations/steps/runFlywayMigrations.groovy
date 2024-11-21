c72799d536aaa8bfc90567e8a83b14da4fa63039
bala547 <balakrishnaulli3@gmail.com>
2024-11-21 17:54:35 +0530

    Update runFlywayMigrations.groovy

--- runflywaymigrations/steps/runFlywayMigrations.groovy
+++ runflywaymigrations/steps/runFlywayMigrations.groovy
@@ -9,9 +9,9 @@ void call() {
                     -v \$(pwd)/migrations:/flyway/sql \\
                     -e JAVA_TOOL_OPTIONS=--add-opens=java.base/java.nio=ALL-UNNAMED \\
                     ${flywayImage} \\
-                    -url=jdbc:snowflake://${SNOWFLAKE_URL}/?db=EMP&schema=BALA \\
-                    -user=${SNOWFLAKE_USERNAME} \\
-                    -password=${SNOWFLAKE_PASSWORD} \\
+                    -url='jdbc:snowflake://${SNOWFLAKE_URL}' \\
+                    -user='${SNOWFLAKE_USERNAME}' \\
+                    -password='${SNOWFLAKE_PASSWORD}' \\
                     -locations=filesystem:/flyway/sql \\
                     migrate
             """
