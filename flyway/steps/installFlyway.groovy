9d56545c219d175d0b6f86f142298f8e43e5b13f
bala547 <balakrishnaulli3@gmail.com>
2024-11-21 11:28:10 +0530

    Update installFlyway.groovy

--- flyway/steps/installFlyway.groovy
+++ flyway/steps/installFlyway.groovy
@@ -1,11 +1,13 @@
 def call() {
     node {
-             withEnv(["JAVA_TOOL_OPTIONS=--add-opens=java.base/java.nio=ALL-UNNAMED"]){
-             sh "rm -rf flyway"
-             sh "wget https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/10.17.3/flyway-commandline-10.17.3-linux-x64.tar.gz"  
-             sh "tar xzvf flyway-commandline-10.17.3-linux-x64.tar.gz"
-             sh "mv flyway-10.17.3 flyway"
-             sh "rm flyway-commandline-10.17.3-linux-x64.tar.gz"
+        withEnv(["JAVA_TOOL_OPTIONS=--add-opens=java.base/java.nio=ALL-UNNAMED"]) {
+            // Run Flyway Docker container with migration
+            sh """
+                docker run --rm \
+                    -v \$(pwd):/flyway/sql \
+                    -v \$(pwd)/conf:/flyway/conf \
+                    flyway/flyway:10.17.3 migrate
+            """
         }
     }
 }
